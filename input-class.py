5b3e189a888654f0c4eec31d190615e100abbf1a
Himesh Ganorkar <ganorkarhimesh67@gmail.com>
2025-04-09 19:52:07 +0530

    Add files via upload

---
+++ input-class.py
@@ -0,0 +1,14 @@
+boxOne = "a"
+boxTwo = "b"
+sum = boxOne + boxTwo
+#product = boxOne * boxTwo
+
+print(sum)
+# print(boxOne)
+
+# print("Enter a number:")
+# number = input()
+# total = number + 11
+# print(total)
+
+#print(number)
